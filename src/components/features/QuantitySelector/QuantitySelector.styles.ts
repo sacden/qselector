e217ad93e33d6713b4be5338db16dd1d3de863e9
sacden <sacden2009@gmail.com>
2026-06-23 12:11:02 +0200

    add: colors

---
+++ src/components/features/QuantitySelector/QuantitySelector.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "@/constants/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.gray,
+  },
+
+  positiveText: {
+    color: COLORS.green,
+  },
+
+  negativeText: {
+    color: COLORS.red,
+  },
+
+  text: {
+    color: COLORS.black,
+  },
+});
---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+export const COLORS = {
+  green: "#4CAF50",
+  red: "#EF5350",
+  gray: "#EBEFF2",
+  black: "#000000",
+} as const;
